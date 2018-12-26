cfeb900dbc061257480c24ec6fbf6c771b189896
sundewang <sundewang@cninfo.com.cn>
2018-12-26 09:15:38 +0800

    复制文件夹为n份

---
+++ copy_directory.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#复制文件夹为n份，文件夹命名方式在原有文件夹后面加数字
+
+# check parameters
+if [ $# -ne 2 ]
+then
+    echo Please input directory path and number of directory
+else
+    srcDir=$1
+    total=$2
+    
+    for (( i = 0; i < total; i++ ))
+    do
+        cp -r $srcDir $srcDir$i
+    done
+
+    echo copy finish
+fi
+
