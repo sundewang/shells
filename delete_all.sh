d64906c0009d5f49b601b82bf0de47f254ba1a62
sundewang <sundewang@cninfo.com.cn>
2019-01-04 16:01:20 +0800

    delete directory

--- delete_all.sh
+++ delete_all.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 # delete all files in the directory
 
+# check parameters
+if [ $# -ne 2 ]
+then
+    echo Please input directory and file name
+    exit 1
+fi
+
 dir=$1
-file=$2
+name=$2
 
 for d in $dir/* ; do
     if [ -d $d ]; then
         for f in $d/* ; do
-            if [ -f $f ] && [[ $f = $d/$file ]] ; then
+            if [[ $f = $d/$name ]] ; then
                 echo $f
-                rm $f
+                if [ -d $f ]; then
+                    rm -r $f
+                else
+                    rm -i $f
+                fi
             fi
         done
 
