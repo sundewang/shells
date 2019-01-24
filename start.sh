3d10ef1ca2a6820c44fcff4c309ccb311a2f56f2
sundewang <631460660@qq.com>
2019-01-24 11:12:15 +0800

    添加注释

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# 使用说明：配置jarName即可
+
 jarName=test.jar
 
+# 获取脚本的所在绝对路径
 case "`uname`" in
     Linux)
 		bin_abs_path=$(readlink -f $(dirname $0))
@@ -10,6 +13,7 @@ case "`uname`" in
 		bin_abs_path=`cd $(dirname $0); pwd`
 		;;
 esac
+
 base=$bin_abs_path
 
 if [ -f $base/java.pid ] ; then
@@ -17,7 +21,7 @@ if [ -f $base/java.pid ] ; then
     exit 1
 fi
 
-# run java
+# run java，将标准输出写到文件，将标准错误写到标准输出中
 java -jar $jarName  1>>$base/logs/nohup.log 2>&1 &
 
 PIDS=$(ps ax | grep -i $jarName | grep java | grep -v grep | awk '{print $1}')
