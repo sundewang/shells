8f31bb4ad505508d0878ecc7dccfb224dea48222
sundewang <631460660@qq.com>
2019-01-24 14:12:08 +0800

    程序执行前，检查是否已经启动

--- start.sh
+++ start.sh
@@ -16,15 +16,16 @@ esac
 
 base=$bin_abs_path
 
-if [ -f $base/java.pid ] ; then
-	echo "found java.pid, Please run stop.sh first, then start.sh"
+# 检查程序是否已经运行
+PIDS=$(ps ax | grep -i $jarName | grep java | grep -v grep | awk '{print $1}')
+
+if [ -n $PIDS ]; then
+    echo "program is running, please run stop.sh first, then start"
     exit 1
 fi
 
 # run java，将标准输出写到文件，将标准错误写到标准输出中
 java -jar $jarName  1>>$base/logs/nohup.log 2>&1 &
 
-PIDS=$(ps ax | grep -i $jarName | grep java | grep -v grep | awk '{print $1}')
-
-echo $PIDS > $base/java.pid
+echo "start program successfully"
 
