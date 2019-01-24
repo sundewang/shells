7788b9c07a39bcd5be252cab9217d54e8ce3fc9e
sundewang <sundewang@cninfo.com.cn>
2019-01-24 15:51:45 +0800

    redirect echo statement in shell

--- start.sh
+++ start.sh
@@ -16,6 +16,9 @@ esac
 
 base=$bin_abs_path
 
+exec 1> $base/out.log
+exec 2> $base/error.log
+
 # 检查程序是否已经运行
 PIDS=$(ps ax | grep -i $jarName | grep java | grep -v grep | awk '{print $1}')
 # 当程序不存在，PIDS未初始化，使用-n判断就会出错
@@ -27,7 +30,7 @@ fi
 # 切换到程序目录
 cd $base
 # run java，将标准输出写到文件，将标准错误写到标准输出中
-java -jar $jarName  1>>$base/logs/nohup.log 2>&1 &
+java -jar $jarName  1>>$base/nohup.log 2>&1 &
 
 echo "start program successfully"
 
--- stop.sh
+++ stop.sh
@@ -4,6 +4,21 @@
 
 jarName=test.jar
 
+# 获取脚本的所在绝对路径
+case "`uname`" in
+    Linux)
+		bin_abs_path=$(readlink -f $(dirname $0))
+		;;
+	*)
+		bin_abs_path=`cd $(dirname $0); pwd`
+		;;
+esac
+
+base=$bin_abs_path
+
+exec 1> $base/out.log
+exec 2> $base/error.log
+
 # 获取程序运行pid
 PIDS=$(ps ax | grep -i $jarName | grep java | grep -v grep | awk '{print $1}')
 
