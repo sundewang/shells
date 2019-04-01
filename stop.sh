a8985a58ce2031a4a7854eafcf04009681a3e4b3
sundewang <sundewang@cninfo.com.cn>
2019-04-01 15:12:32 +0800

    fix #1

--- stop.sh
+++ stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 使用说明：配置jarName即可
-
-jarName=test.jar
+# 使用说明：直接运行即可
+# 引入库文件
+. ./getJarName.sh
 
 # 获取脚本的所在绝对路径
 case "`uname`" in
@@ -15,6 +15,16 @@ case "`uname`" in
 esac
 
 base=$bin_abs_path
+
+# 获取路径中jar文件，如果有超过两个jar文件或者返回-1，退出程序
+jarArray=($(getJarName $base))
+if [ ${#jarArray[*]} -ne 1 ] || [ ${jarArray[0]} -eq -1 ]; then
+    echo "please check file in directory first, then start"
+    exit 1;
+fi
+
+jarName=${jarArray[0]}
+echo "jar file name is $jarName"
 
 exec 1> $base/out.log
 exec 2> $base/error.log
